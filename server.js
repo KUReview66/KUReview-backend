dee1268880bb03d7748d9b15ddf9545e07d6a2da
PloyyNK <nartnatta_kri@yahoo.com>
2025-02-14 04:32:33 +0700

    login and mock api student score

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const sql = require("mssql");
 const axios = require('axios');
 const qs = require('qs');
+const crypto = require('crypto');
 require("dotenv").config();  
 
 const app = express();
@@ -30,9 +31,29 @@ sql.connect(config, err => {
     console.log("Connection Successful!");
 });
 
+function encryptAES(text, key) {
+    const iv = crypto.randomBytes(16); 
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv);
+    let encrypted = cipher.update(text, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+    
+    return iv.toString('base64') + ':' + encrypted; 
+}
+
 app.get("/", async (req, res) => {
     res.send("KUReview Backend Server");
+    const key = crypto.randomBytes(32).toString('hex'); 
+    const plaintext = "b6410545509";
+
+    const encryptedText = encryptAES(plaintext, key);
+    // res.send(encryptedText, key)
+    console.log("Encrypted:", encryptedText);
+    console.log("Key (keep this safe!):", key);
 });
+
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+})
 
 app.get("/token", async (req, res) => {
     axios
@@ -48,6 +69,28 @@ app.get("/token", async (req, res) => {
     .catch((error) => {
         console.error('Error:', error.response ? error.response.data : error.message);
     });
+})
+
+app.get("/student-score/topic-wise", async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request().query('SELECT * FROM studentScoreTopicWise');
+        res.send(result.recordset);
+        pool.close();
+    } catch (err) {
+        console.error('SQL Error', err);
+    }
+})
+
+app.get("/student-score/level-wise", async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request().query('SELECT * FROM classificationLevelWise');
+        res.send(result.recordset);
+        pool.close();
+    } catch (err) {
+        console.error('SQL Error', err);
+    }
 })
 
 app.get("/suggest", async (req, res) => {
@@ -68,7 +111,7 @@ app.get("/suggest/:id", async (req, res) => {
 
         const result = await pool
             .request()
-            .input('id', sql.Int, id)
+            .input('id', sql.BigInt, id)
             .query('SELECT * FROM suggestContent WHERE id = @id');
         if (result.recordset.length === 0) {
             res.status(404).send({ message: "No record found with the given ID" });
@@ -81,18 +124,40 @@ app.get("/suggest/:id", async (req, res) => {
     }
 })
 
+app.get("/suggest/:id/:round/:unit", async (req, res) => {
+    const { id, round, unit } = req.params;
+    try {
+        const pool = await sql.connect(config);
+
+        const result = await pool
+            .request()
+            .input('id', sql.BigInt, id)
+            .input('round', sql.Int, round)
+            .input('unit', sql.NVarChar, unit) 
+            .query('SELECT * FROM suggestContent WHERE id = @id AND round = @round AND CAST(unit AS NVARCHAR(MAX)) = @unit');
+
+        res.send(result.recordset);
+        pool.close();
+    } catch (err) {
+        console.error('SQL error', err);
+        res.status(500).send('Internal server error');
+    }
+});
+
+
 app.post("/suggest", async (req, res) => {
     try {
-        const {id, round, content} = req.body;
+        const {id, round, unit, content} = req.body;
     
         const pool = await sql.connect(config);
     
         const result = await pool.request()
-            .input('id', sql.Int, id)
+            .input('id', sql.BigInt, id)
             .input('round', sql.Int, round)
+            .input('unit', sql.Text, unit)
             .input('content', sql.Text, content)
             .query(
-                'INSERT INTO suggestContent (id, round, content) VALUES (@id, @round, @content)'
+                'INSERT INTO suggestContent (id, round, unit, content) VALUES (@id, @round, @unit, @content)'
             );
         
         res.status(201).send({
